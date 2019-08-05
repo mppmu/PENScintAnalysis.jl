79aa6019066f78af85b1b6b983f5382299588683
Oliver Schulz <oschulz@mpp.mpg.de>
2019-08-05 10:43:13 +0200

    Add runtests

---
+++ test/runtests.jl
@@ -0,0 +1,8 @@
+# This file is a part of PENScintAnalysis.jl, licensed under the MIT License (MIT).
+
+import Test
+Test.@testset "Package PENScintAnalysis" begin
+
+# test code goes here
+
+end # testset
