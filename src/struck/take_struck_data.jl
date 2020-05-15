d05ac481e3cab726a1829736a5dba78fc58b78d7
Felix Fischer <ffischer@mpp.mpg.de>
2020-05-15 14:09:03 +0200

    add info notice

--- src/struck/take_struck_data.jl
+++ src/struck/take_struck_data.jl
@@ -29,6 +29,7 @@ Creates an individual `pmt_daq.scala` file and takes data which are converted to
 ...
 """
 function take_struck_data(settings::NamedTuple; calibration_data::Bool=false)
+    @info("Updated: 2020-05-15 14:08")
     if !isdir(settings.data_dir)
         mkpath(settings.data_dir, mode = 0o777)
     end
