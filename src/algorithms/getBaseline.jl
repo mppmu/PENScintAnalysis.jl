f3edf88a2b893300d6592f9b518d3ba4d0be051f
Felix Fischer <ffischer@mpp.mpg.de>
2021-04-13 12:22:13 +0200

    Added variable threshold

--- src/algorithms/getBaseline.jl
+++ src/algorithms/getBaseline.jl
@@ -4,12 +4,13 @@ function to compute the baseline of a given wf, it takes a wf and compute the av
 removing +/- 5 samples around the peaks to avoid bias
  Arguments
 - signal::Vector: Waveform to be analyzed
+- threshold: Threshold for peaks. Default = 15.0
 ...
 """
 
-function getBaseline(signal::Vector)
+function getBaseline(signal::Vector; threshold = 15.0)
     Baseline = copy(signal)
-    peaks_threshold = mean(Baseline) + 15.0 # only peaks with amplitudes 15 units larger than the average
+    peaks_threshold = mean(Baseline) + threshold # only peaks with amplitudes threshold units larger than the average
     peak_pos = findLocalMaxima(Baseline,peaks_threshold)
     #peak_pos = findall(x -> x > peaks_threshold, signal)
     index_to_delete = []
