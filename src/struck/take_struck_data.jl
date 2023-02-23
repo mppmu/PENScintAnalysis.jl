60c34954c03a0f5e96bb7c29bef6f4351980032a
David Hervas <hervasa2@live.unc.edu>
2023-02-23 15:13:27 +0100

    Automatic process retries (max. 3 times total)

--- src/struck/take_struck_data.jl
+++ src/struck/take_struck_data.jl
@@ -13,8 +13,8 @@ stop_taking is a function that will be called before any iteration. If it return
 # Example settings
 - `settings = (fadc = "gelab-fadc08",` 
 `output_basename = "test-measurement",`
-`data_dir = "../data/",`
-`conv_data_dir = "../conv_data/",`
+`data_dir = "./data/",`
+`conv_data_dir = "./conv_data/",`
 `measurement_time = 20,`
 `number_of_measurements = 5,`
 `channels = [1,2,3,4,5,6],`
@@ -34,7 +34,7 @@ stop_taking is a function that will be called before any iteration. If it return
 ...
 """
 function take_struck_data(settings::NamedTuple; calibration_data::Bool=false, callback=false, stop_taking = ()->false)
-    @info("Updated: 2023-02-13")
+    @info("Updated: 2023-02-23")
     !isdir(settings.data_dir) ? mkpath(settings.data_dir, mode = 0o775) : "Path exists"
     
     !isdir(settings.conv_data_dir) ? mkpath(settings.conv_data_dir, mode = 0o775) : "Path exists"
@@ -48,8 +48,12 @@ function take_struck_data(settings::NamedTuple; calibration_data::Bool=false, ca
      #   settings.filter_faulty_events = false
     end
     
-    current_dir = pwd()
-    cd(settings.data_dir)
+    # Get current working directory and normalized paths of data+conv_data dirs
+    original_dir = pwd()
+    data_dir = normpath(original_dir, settings.data_dir)
+    conv_data_dir = normpath(original_dir, settings.conv_data_dir)
+
+    cd(data_dir)
     create_struck_daq_file(settings, calibration_measurement=calibration_data)
     
     t_check = stat("pmt_daq_dont_move.scala").mtime
@@ -60,40 +64,49 @@ function take_struck_data(settings::NamedTuple; calibration_data::Bool=false, ca
     i = 1
 
     while i <= settings.number_of_measurements && !stop_taking()
-        @info "Iteration " * string(i) * " started"
-        cd(settings.data_dir)
-        #chmod("./", 0o775)
+        @info "Chunk " * string(i) * " - Receiving"
 
-        # Maximum time we expect the script to run: Twice the measurement time; after that, throw exception
-        t_start = now()
-        process = run(`./pmt_daq_dont_move.scala`, wait=false)
-        while (now() - t_start).value < 2000*settings.measurement_time
-            if process_exited(process)
+        # Total tries: 3
+        n_try = 0
+        while n_try <= 2
+            @info "Try " * string(n_try + 1) * "/3"
+
+            # Maximum time we expect the script to run: 1.5*measurement_time; after that, throw exception
+            t_start = now()
+            process = run(`./pmt_daq_dont_move.scala`, wait=false)
+            while (now() - t_start).value < 1.5*1000*settings.measurement_time
+                if process_exited(process)
+                    break
+                end
+                sleep(0.5)
+            end
+
+            # If process finised within time, no retries neccessary
+            if process_running(process)
+                @warn "Process timeout. " * ((n_try <= 2) ? "Trying again" : "Stopping. See below error message")
+                kill(process)
+                
+                n_try += 1
+            else
                 break
             end
-            sleep(0.5)
         end
 
-        if process_running(process)
-            kill(process)
+        # n_try = 4 => previous 3 tries failed
+        if n_try == 4
             rm("pmt_daq_dont_move.scala")
-            cd(current_dir)
+            cd(original_dir)
 
             @error "Process timed out"
-            throw(ErrorException("Process timed out"))            
+            throw(ErrorException("Measurement failed. Make sure the fadc is running, you're running the code within the legend(-base) container on glab-pc01 and correct permissions are set on the directory. If you can ping the struck and think everything is set up correctly, do a test using struck-test-gui. Execute that on a Linux host with Desktop functionality connecting via ssh -X gelab@gelab-pc.. and check connection as well as Test in the sub-menu. This helps to spin up a Struck if a restart alone did not help"))            
         end
         
-        @info "Iteration " * string(i) * " finished"
-        #ProgressMeter.next!(p)
-        i += 1
+        @info "Chunk " * string(i) * " - Received"
 
         # Update list of new files after each measurement for callback usage
-        cd(current_dir)
-        files = Glob.glob(joinpath(settings.data_dir, settings.output_basename * "*.dat"))
+        files = Glob.glob(joinpath(settings.output_basename * "*.dat"))
 
-        @info "Getting name of created file"
-    
-        # Assumption: Only one file will be added per execution. Otherwise we'd to sort files by file_change_time and get all items with file_change_time > t_check
+        # Assumption: Only one file will be added per execution. Otherwise we'd have to sort files by file_change_time and get all items with file_change_time > t_check
         j = 1
         new_file = ""
         while j <= length(files)
@@ -108,8 +121,6 @@ function take_struck_data(settings::NamedTuple; calibration_data::Bool=false, ca
             j += 1
         end
 
-        @info "Finalized iteration " *  string(i)
-
         if callback != false
             @info "Received chunk " * new_file
             data = read_data_from_struck(new_file, filter_faulty_events=settings.filter_faulty_events, coincidence_interval = settings.coincidence_interval)
@@ -117,14 +128,11 @@ function take_struck_data(settings::NamedTuple; calibration_data::Bool=false, ca
         end
     end
 
-    @info "Hello World"
-
     #chmod(pwd(), 0o775, recursive=true)
-    cd(settings.data_dir)
     rm("pmt_daq_dont_move.scala")
-    cd(current_dir)
     
     if !settings.skip_post_processing
+        @info "Doing analysis"
         limit   = settings.h5_filesize_limit
         h5size  = 0
         h5files = []
@@ -136,6 +144,7 @@ function take_struck_data(settings::NamedTuple; calibration_data::Bool=false, ca
             if i <= length(new_files)
                 while h5size <= limit && i <= length(new_files)
                     h5size += stat(new_files[i]).size/1024^2
+                    @info "Found file " * string(h5size)
                     push!(compress, i)
                     i += 1
                 end
@@ -143,14 +152,20 @@ function take_struck_data(settings::NamedTuple; calibration_data::Bool=false, ca
             push!(h5files, compress)
         end
 
+        print(h5files)
+
+        @info "Conversion"
+
+        print(new_files)
+
         i = 1
         p = ProgressMeter.Progress(length(h5files), 1, "Converting "*string(length(new_files))*" files to "*string(length(h5files))*" HDF5...", 50)
         while i <= length(h5files)
             data = read_data_from_struck(new_files[h5files[i]], filter_faulty_events=settings.filter_faulty_events, coincidence_interval = settings.coincidence_interval)
             if calibration_data
-                writeh5(joinpath(settings.conv_data_dir, "calibration-data_" * split(basename(new_files[h5files[i][1]]), ".dat")[1] * ".h5"), data)
+                writeh5(joinpath(conv_data_dir, "calibration-data_" * split(basename(new_files[h5files[i][1]]), ".dat")[1] * ".h5"), data)
             else
-                writeh5(joinpath(settings.conv_data_dir, split(basename(new_files[h5files[i][1]]), ".dat")[1] * ".h5"), data)
+                writeh5(joinpath(conv_data_dir, split(basename(new_files[h5files[i][1]]), ".dat")[1] * ".h5"), data)
             end
             
             ProgressMeter.next!(p)
@@ -166,6 +181,6 @@ function take_struck_data(settings::NamedTuple; calibration_data::Bool=false, ca
         end
     end
     
-    cd(current_dir)
+    cd(original_dir)
 end
 export take_struck_data
